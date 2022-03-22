937481b118c635ef9728a45d60247b1c116bff91
the-alex-g <alex.gestwicki@gmail.com>
2022-03-22 10:31:09 -0400

    Make game restart use timer instead of yield:

--- project/Main/Main.gd
+++ project/Main/Main.gd
@@ -3,13 +3,17 @@ extends Node2D
 signal update_player_health(new_value, player_id)
 signal game_over(loser_id)
 
+onready var _game_over_timer = $GameOverTimer as Timer
 
 func _on_Player_dead(player:int)->void:
 	emit_signal("game_over", player)
-	yield(get_tree().create_timer(2.0), 'timeout')
-	# warning-ignore:return_value_discarded
-	get_tree().change_scene("res://Main/Main.tscn")
+	_game_over_timer.start()
 
 
 func _on_Player_update_health(new_value:int, player:int)->void:
 	emit_signal("update_player_health", new_value, player)
+
+
+func _on_GameOverTimer_timeout()->void:
+	# warning-ignore:return_value_discarded
+	get_tree().change_scene("res://Main/Main.tscn")
